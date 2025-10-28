e9b1da608b4b51160180b9eecb0f611a8ab7501a
Vino <VinoshenSilva@gmail.com>
2025-10-28 15:56:03 +0800

    Polish UI

--- src/components/Task/TaskItem.tsx
+++ src/components/Task/TaskItem.tsx
@@ -32,13 +32,10 @@ const TaskItem = ({
   onUpdate,
   isDaily,
 }: TaskItemProps) => {
-  return (
-    <div
-      draggable
-      className="w-full md:min-w-xs md:max-w-xs shadow-md shadow-primary-black rounded-md border p-4 flex flex-col gap-3"
-    >
+  const elem = (
+    <div className="w-full md:min-w-md md:max-w-md shadow-md shadow-primary-black rounded-md border p-4 flex flex-col gap-3">
       <div className="flex justify-between gap-5">
-        <div className="flex w-full items-center">
+        <div className="flex gap-4 w-full items-center">
           {/* <Input
             onChange={(e) => {
               onUpdate({ isCompleted, name: e.target.value, isDaily, points });
@@ -47,6 +44,34 @@ const TaskItem = ({
             placeholder="Task name"
             className="full px-1 py-1"
           /> */}
+          <div className="flex gap-2">
+            <Tooltip content={"Is it complete?"}>
+              <Checkbox
+                checked={isCompleted}
+                onChange={(e) => {
+                  onUpdate({
+                    isCompleted: e.target.checked,
+                    name,
+                    isDaily,
+                    points,
+                  });
+                }}
+              />
+            </Tooltip>
+            <Tooltip content={"Is it a daily task?"}>
+              <Checkbox
+                checked={isDaily}
+                onChange={(e) => {
+                  onUpdate({
+                    isCompleted,
+                    name,
+                    isDaily: e.target.checked,
+                    points,
+                  });
+                }}
+              />
+            </Tooltip>
+          </div>
           <p className="w-full">{name}</p>
           <div className="w-20 ml-4">
             <Tooltip content={"Points"}>
@@ -66,54 +91,29 @@ const TaskItem = ({
               />
             </Tooltip>
           </div>
-        </div>
-      </div>
-      <div className="flex gap-4 justify-between items-start">
-        <div className="flex gap-2">
-          <Checkbox
-            checked={isCompleted}
-            onChange={(e) => {
-              onUpdate({
-                isCompleted: e.target.checked,
-                name,
-                isDaily,
-                points,
-              });
-            }}
-            label="Complete"
-          />
-          <Checkbox
-            checked={isDaily}
-            onChange={(e) => {
-              onUpdate({
-                isCompleted,
-                name,
-                isDaily: e.target.checked,
-                points,
-              });
-            }}
-            label="Daily Task"
-          />
-        </div>
-        <div className="flex gap-2">
-          {description ? <Tooltip content={description} /> : <></>}
-          <Confirm
-            message="Are you sure you want to delete this task?"
-            onConfirm={() => {
-              if (onClickDelete) {
-                onClickDelete();
-              }
-            }}
-            onCancel={() => console.log("Cancelled")}
-          >
-            <IconButton className="p-1">
-              <FaTrash className="text-xs" />
-            </IconButton>
-          </Confirm>
+          <div className="flex gap-2 items-center">
+            <Confirm
+              message="Are you sure you want to delete this task?"
+              onConfirm={() => {
+                if (onClickDelete) {
+                  onClickDelete();
+                }
+              }}
+              onCancel={() => console.log("Cancelled")}
+            >
+              <IconButton className="p-1">
+                <FaTrash className="text-xs" />
+              </IconButton>
+            </Confirm>
+          </div>
         </div>
       </div>
     </div>
   );
+
+  if (!description) return elem;
+
+  return <Tooltip content={description}>{elem}</Tooltip>;
 };
 
 export default TaskItem;
